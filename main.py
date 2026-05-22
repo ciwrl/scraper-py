648d26160130062478267baaf171a6fc2930c9c5
ciwrl <ciwrl05@gmail.com>
2026-05-22 15:37:17 -0500

    Bootstrap main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import sys
+# uv run main.py BASE_URL
+
 def main():
-    print("Hello from scraper-py!")
+    if len(sys.argv) < 2:
+        print("no website provided")
+        sys.exit(1)
+    if len(sys.argv) > 2:
+        print("too many arguments provided")
+        sys.exit(1)
+    print("starting crawl of:", sys.argv[1]) # BASE_URL
 
 
 if __name__ == "__main__":
